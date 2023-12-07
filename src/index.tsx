f92aaa1fc4836e4f07c6a94b2d735ad500f2c2f0
nick_hart <nhartmusic@gmail.com>
2023-12-07 14:31:55 -0800

    router working

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import * as ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
     <Router>
