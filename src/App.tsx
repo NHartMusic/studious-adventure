cb36a2f5d6250bc0702b78bead087490276ebe57
nick_hart <nhartmusic@gmail.com>
2023-12-09 16:31:38 -0800

    experimenting with gradient background

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Routes, Route } from "react-router-dom"
+import styled from 'styled-components'
 //nav
 import Header from './Navigation/Header'
 //components
@@ -8,17 +9,25 @@ import Music from './Pages/Music/index'
 import Tech from './Pages/Tech/index'
 import Connect from './Pages/Connect/index'
 
+
+const ContentWrapper = styled.div`
+  height: 100%;
+  flex-direction: column; 
+`
+
 function App() {
  
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/Music' element={<Music />} />
-        <Route path='/Tech' element={<Tech />} />
-        <Route path='/Connect' element={<Connect />} />
-      </Routes>
+      <ContentWrapper>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Music' element={<Music />} />
+          <Route path='/Tech' element={<Tech />} />
+          <Route path='/Connect' element={<Connect />} />
+        </Routes>
+      </ContentWrapper>
     </>
   )
 }
