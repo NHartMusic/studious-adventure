85d9be7b2bd1f3b9bc4f22026121260a547eb565
nick_hart <nhartmusic@gmail.com>
2024-01-11 20:15:21 -0800

    routing looking correct

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 
+//dependencies 
 import * as React from 'react'
 import { Routes, Route } from "react-router-dom"
 import styled from 'styled-components'
 //nav
 import Header from './Navigation/Header'
 import Footer from './Navigation/Footer'
-//components
+//pages
+import Albums2023List from './Pages/Blog/Posts/Albums2023/Albums2023List'
 import Blog from './Pages/Blog/index'
 import Connect from './Pages/Connect/index'
 import Home from './Pages/Home/index'
@@ -25,7 +27,9 @@ function App() {
       <ContentWrapper>
         <Header />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={<Home />} /> 
+          <Route path='/bestAlbumsOf2023' element ={<Albums2023List />} />
+          <Route path='/bestFilmsOf2023' />
           <Route path='/Blog' element={<Blog />} />
           <Route path='/Connect' element={<Connect />} />
           <Route path='/Music' element={<Music />} />
--- src/Navigation/Footer.tsx
+++ src/Navigation/Footer.tsx
@@ -1,14 +1,15 @@
-import styled from 'styled-components'
-import MainNav from './MainNav'
-import HomeNav from './HomeNav'
-import SocialNav from './SocialNav'
 
+//component imports
+import HomeNav from './HomeNav'
+//styles imports
+import { blueList } from '../UI/Colors/Blues'
+import styled from 'styled-components'
 import { MenuBreakpoint } from '../UI/MediaQueries'
 
 const FooterWrapper = styled.div`
     display: flex;
     margin: 20px 10px;
-    background-color: blue;
+    background-color: ${blueList[8]};
 
      @media ${MenuBreakpoint} {
         display: block !important;
